ad363f274b41fd8a769840402e9d759f14d0ee80
chupichapa <pasionharold01@gmail.com>
2024-12-21 15:50:25 +0800

    yes sir
    dsadsa

--- server/generate_hash.php
+++ server/generate_hash.php
@@ -1,4 +1,4 @@
 <?php
-$password = '@dmin2';  // Replace this with the actual password you want to use
+$password = '@dmin3';  // Replace this with the actual password you want to use
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 echo $hashed_password;
